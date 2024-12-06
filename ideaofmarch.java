8ffb48db26e4aa20fd8a93f55b250ded8b50970a
assu786 <shaikaqsa.78@gmail.com>
2024-12-06 21:21:59 +0530

    Add files via upload

---
+++ ideaofmarch.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Codechef
+{
+	public static void main (String[] args) 
+	{
+	Scanner n=new Scanner(System.in);
+	int N=n.nextInt();
+	if (N==15)
+	{
+	    System.out.println("Yes");
+	}
+	else{
+	    System.out.println("NO");
+	}
+
+	}
+}
