9646d516586fffe863983debf7f8083b95139564
assu786 <shaikaqsa.78@gmail.com>
2024-12-06 21:22:44 +0530

    Add files via upload

---
+++ moneymaking.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Codechef
+{
+	public static void main (String[] args) 
+	{
+	Scanner n=new Scanner (System.in);
+	int x=n.nextInt();
+	int y=n.nextInt();
+	int sell=x*5000+y*9800;
+	System.out.println(sell);
+
+	}
+}
