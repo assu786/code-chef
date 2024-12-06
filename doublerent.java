9df173db36e1fc644aa99a1c9b002419525d8da0
assu786 <shaikaqsa.78@gmail.com>
2024-12-06 21:56:21 +0530

    Add files via upload

---
+++ doublerent.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Codechef
+{
+	public static void main (String[] args) 
+	{
+	Scanner n=new Scanner(System.in);
+	int X = n.nextInt();
+	int y=X*2;
+	System.out.println(y);
+
+	}
+}
