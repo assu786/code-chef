471881316bfbe62bcb8135c8f4033a47b3e2cc20
assu786 <shaikaqsa.78@gmail.com>
2024-12-06 21:19:49 +0530

    Add files via upload

---
+++ codechefround.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Codechef
+{
+	public static void main (String[] args) 
+	{
+	Scanner n=new Scanner(System.in);
+	int N=n.nextInt();
+	if(N==4)
+	{
+	    System.out.println("YES");
+	}
+	else
+	{
+	    System.out.println("NO");
+
+	}
+	}
+}
