80673d3fa28c641a970d504b6a5584b4a718bd04
assu786 <shaikaqsa.78@gmail.com>
2024-12-06 21:21:07 +0530

    Add files via upload

---
+++ goldcoin.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class Codechef
+{
+	public static void main (String[] args) 
+	{
+	Scanner n=new Scanner (System.in);
+	int A=n.nextInt();
+	int B=n.nextInt();
+	int X=n.nextInt();
+	int Y=n.nextInt();
+	if(X>Y)
+	{
+	    System.out.println(A);
+	}
+	else{
+	    System.out.println(B);
+	}
+
+	}
+}
