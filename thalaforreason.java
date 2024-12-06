90d0161ac2ce00b326989e8b60ef071da4b199b3
assu786 <shaikaqsa.78@gmail.com>
2024-12-06 21:25:57 +0530

    Add files via upload

---
+++ thalaforreason.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Codechef
+{
+	public static void main (String[] args) 
+	{
+	Scanner n=new Scanner (System.in);
+	int N=n.nextInt();
+	if (N==7)
+	System.out.println("THALA");
+	else
+	System.out.println("SADGE");
+	
+
+	}
+}
