1e776709be014e00dbf9e621903b04bf1bec3fca
assu786 <shaikaqsa.78@gmail.com>
2024-12-06 22:24:46 +0530

    Add files via upload

---
+++ sweetsshop.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Codechef
+{
+	public static void main (String[] args) t
+	{
+	Scanner sc=new Scanner (System.in);
+	int X=sc.nextInt();
+	int N=sc.nextInt();
+	int laddu=10;
+	int jilebi=20;
+	int z=X-(N*laddu);
+	int a=z/jilebi;
+	System.out.println(a);
+
+	}
+}
