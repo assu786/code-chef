89097785c3a47e14a89663e4f14dd1e1ce7cfe90
assu786 <shaikaqsa.78@gmail.com>
2024-12-06 22:13:35 +0530

    Add files via upload

---
+++ acplz.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Codechef
+{
+	public static void main (String[] args) 
+	{
+	Scanner n=new Scanner(System.in);
+	int T=n.nextInt();
+	if(T>30)
+	System.out.println("YES");
+	else
+	System.out.println("NO");
+
+	}
+}
