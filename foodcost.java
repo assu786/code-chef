0fab2b584ade074f6cc1d40ecc37f1b7a3b9a451
assu786 <shaikaqsa.78@gmail.com>
2024-12-06 21:20:15 +0530

    Add files via upload

---
+++ foodcost.java
@@ -0,0 +1,13 @@
+class Codechef
+{
+	public static void main (String[] args) 
+	{
+		Scanner sc=new Scanner (System.in);
+		int X=sc.nextInt();
+		int Y=sc.nextInt();
+		int z=X*6+Y;
+		System.out.println(z);
+
+	}
+}
+	
