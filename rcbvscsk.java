f9a71df5ce170a87e8adefa5cac98c93f6b4de70
assu786 <shaikaqsa.78@gmail.com>
2024-12-06 21:24:05 +0530

    Add files via upload

---
+++ rcbvscsk.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Codechef
+{
+	public static void main (String[] args) 
+	{
+	    Scanner n=new Scanner(System.in);
+	    int a=n.nextInt();
+	    int b=n.nextInt();
+	    if((a-b)>=18)
+	    System.out.println("RCB");
+	    else
+	    System.out.println("CSK");
+
+	}
+}
