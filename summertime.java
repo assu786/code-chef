6beb0e63547b49d4781bb5b55c3905048940f2ae
assu786 <shaikaqsa.78@gmail.com>
2024-12-06 21:24:32 +0530

    Add files via upload

---
+++ summertime.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Codechef
+{
+	public static void main (String[] args) 
+	{
+	Scanner n=new Scanner (System.in);
+	int x=n.nextInt();
+	if(x>35)
+	System.out.println("yes");
+	else 
+	System.out.println("NO");
+
+	}
+}
