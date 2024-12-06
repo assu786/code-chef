f86ccffca8ecd2664d9e6455912e4eb0d6eb0df6
assu786 <shaikaqsa.78@gmail.com>
2024-12-06 21:23:18 +0530

    Add files via upload

---
+++ puzhunt.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Codechef
+{
+	public static void main (String[] args) 
+	{
+	Scanner c=new Scanner (System.in);
+	int N=c.nextInt();
+	if(N>=6&&N<=8)
+	{
+	    System.out.println("Yes");
+	}
+	else
+	{
+	    System.out.println("No");
+	}
+    
+
+	}
+}
