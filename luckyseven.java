eedbbf1ef13878c2dc4f57bbd9885ddc8e6e8ab7
assu786 <shaikaqsa.78@gmail.com>
2024-12-06 21:22:20 +0530

    Add files via upload

---
+++ luckyseven.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Codechef
+{
+	public static void main (String[] args) 
+	{
+	Scanner n=new Scanner(System.in);
+	String S=n.nextLine();
+	char luckynumber=S.charAt(6);
+	System.out.println(luckynumber);
+
+	}
+}
