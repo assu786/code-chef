3fae2d201c8add6ce91e0ba57c38e3b24b4cc269
assu786 <shaikaqsa.78@gmail.com>
2024-12-06 21:19:29 +0530

    Add files via upload

---
+++ christmasgreetings.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Codechef
+{
+	public static void main (String[] args) 
+	{
+		Scanner n=new Scanner (System.in);
+	    int a=n.nextInt();
+	    if (a==25)
+	    System.out.println("CHRISTMAS");
+	    else
+	    System.out.println("ORDINARY");
+
+	}
+}
