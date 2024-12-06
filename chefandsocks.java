afcd2212b43b322ee8236409d5ad05c2f81cf9c4
assu786 <shaikaqsa.78@gmail.com>
2024-12-06 21:18:50 +0530

    Add files via upload

---
+++ chefandsocks.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Codechef
+{
+	public static void main (String[] args) 
+	{
+	Scanner n=new Scanner (System.in);
+	int A=n.nextInt();
+	int x=n.nextInt();
+	int y=n.nextInt();
+	int c=x+y;
+	if(A<=c)
+    System.out.println("YES");
+    else
+    System.out.println("NO");
+
+	}
+}
