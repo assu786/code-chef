84536a6e42867b5d3f13a0aed8a5db8a9e4576e8
assu786 <shaikaqsa.78@gmail.com>
2024-12-06 21:18:20 +0530

    Add files via upload

---
+++ AICOM.java
@@ -0,0 +1,13 @@
+class Codechef
+{
+	public static void main (String[] args) 
+	{
+		Scanner sc=new Scanner (System.in);
+		int X=sc.nextInt();
+		if(X<=60)
+		System.out.println("YES");
+		else
+		System.out.println("NO");
+
+	}
+}
