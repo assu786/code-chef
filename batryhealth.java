f1695aa3e884d41619b4cc097b412e5c347b7003
assu786 <shaikaqsa.78@gmail.com>
2024-12-08 20:06:06 +0530

    Add files via upload

---
+++ batryhealth.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Codechef
+{
+	public static void main (String[] args) 
+	{
+	Scanner s=new Scanner (System.in);
+	int T=s.nextInt();
+    for (int i=1;i<=T;i++)
+	{
+	    int X=s.nextInt();
+	    if(X>=80)
+	    System.out.println("YES");
+	    else 
+	    System.out.println("NO");
+	}
+
+	}
+}
