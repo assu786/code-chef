5134b79deb587f2f07338aa991ceceef742819bf
assu786 <shaikaqsa.78@gmail.com>
2024-12-08 20:07:36 +0530

    Add files via upload

---
+++ overspeed.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Codechef
+{
+	public static void main (String[] args) 
+	{
+	Scanner n=new Scanner(System.in);
+	int S=n.nextInt();
+	if(S>40)
+	System.out.println("YES");
+	else
+	System.out.println("NO");
+
+	}
+}
