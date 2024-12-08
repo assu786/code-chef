23bb0d50bcfd1fe0e962887ac503344c774bd5c0
assu786 <shaikaqsa.78@gmail.com>
2024-12-08 20:33:59 +0530

    Add files via upload

---
+++ luckyclover.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Codechef
+{
+	public static void main (String[] args) 
+	{
+	Scanner sc=new Scanner(System.in);
+	int N=sc.nextInt();
+	int z=4+3*(N-1);
+	System.out.println(z);
+
+	}
+}
