d126a8d53e53827f247125660e82e9bb8bb76e2f
assu786 <shaikaqsa.78@gmail.com>
2024-12-08 20:04:55 +0530

    Add files via upload

---
+++ advitiya.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Codechef
+{
+	public static void main (String[] args) 
+	{
+	Scanner n=new Scanner (System.in);
+	int N=n.nextInt();
+	if(N>=16&&N<=18)
+	System.out.println("ADVITIYA");
+	else
+	System.out.println("WAITING FOR ADVITIYA");
+
+	}
+}
