9f56dbdf81bd1cf659f496dd3957c4c42e3390cf
assu786 <shaikaqsa.78@gmail.com>
2024-12-08 20:26:40 +0530

    Add files via upload

---
+++ icecream.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Codechef
+{
+	public static void main (String[] args) 
+	{
+	Scanner sc=new Scanner (System.in);
+	int X=sc.nextInt();
+	int Y=sc.nextInt();
+	int z=2*X;
+	if(Y>=z)
+	System.out.println("YES");
+	else 
+	System.out.println("No");
+
+	}
+}
