c463bf46de19e3f9ad62fec00006196a1ea30e84
assu786 <shaikaqsa.78@gmail.com>
2024-12-08 20:09:46 +0530

    Add files via upload

---
+++ heatwave.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Codechef
+{
+	public static void main (String[] args) 
+	{
+	Scanner s=new Scanner(System.in);
+	int X=s.nextInt();
+	int Y=s.nextInt();
+	if(X>=Y)
+	System.out.println("NO");
+	else
+	System.out.println("YES");
+
+	}
+}
